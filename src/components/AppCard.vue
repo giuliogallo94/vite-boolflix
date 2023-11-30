d9205e00aa19acff2b4f818d05f1e38a0ffdb271
giuliogallo94 <giuliogallo1994@gmail.com>
2023-11-30 13:12:59 +0100

    add: stars as vote

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -5,9 +5,23 @@ export default {
   data() {
     return {
       store,
+      finalVote: "",
     };
   },
-  methods: {},
+  methods: {
+    // voteInStars() {
+    //   if (mediaObj.vote_avarage < 1) {
+    //     this.finalVote = `<i v-for="i in 5" class="fa-regular fa-star"></i>`;
+    //   } else {
+    //     this.finalVote = `<i
+    //         v-for="i in Math.ceil(mediaObj.vote_average / 2)"
+    //         class="fa-solid fa-star"></i>
+    //         <i
+    //         v-for="i in (5 - Math.ceil(mediaObj.vote_average / 2))"
+    //         class="fa-regular fa-star"></i>`;
+    //   }
+    // },
+  },
   computed: {
     // hasFlag() {
     //   return this.store.flagsArray.includes(movie.original_language);
@@ -40,13 +54,21 @@ export default {
             class="flag" />
           <p class="m-0" v-else>{{ mediaObj.original_language }}</p>
         </li>
-        <li>Voto: {{ Math.ceil(mediaObj.vote_average) }}</li>
+        <li v-if="mediaObj.vote_average > 0">
+          Voto:
+          <i
+            v-for="i in Math.ceil(mediaObj.vote_average / 2)"
+            class="fa-solid fa-star"></i>
+        </li>
+        <li v-else>Voto: <i v-for="i in 5" class="fa-solid fa-star"></i></li>
+        <!-- <li v-html="voteInStars()"></li> -->
       </div>
     </ul>
   </li>
 </template>
 
 <style lang="scss" scoped>
+@import "@fortawesome/fontawesome-free/css/all.css";
 .info {
   .null-img {
     width: 342px;
